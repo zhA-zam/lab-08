74e4e6a3d6ca2d8db10ff0f46331a943b9f7580a
zhA_zam <zphazam432@gmail.com>
2026-03-06 13:56:24 -0700

    add deleteCity tests

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -31,40 +31,40 @@ public class CustomListTest {
         assertEquals(3, cityList.countCities()); // expected = 4 because mockCity is always in cityList
     }
 
-//    @Test
-//    void testDeleteCity() {
-//        CustomList cityList = new CustomList();
-//
-//        // Test to see if deleteCity actually removes a city from the list
-//        City city1 = new City("Calgary", "Alberta");
-//        City city2 = new City("Regina", "Saskatchewan");
-//        City city3 = new City("Charlottetown", "Prince Edward Island");
-//
-//        cityList.addCity(city1);
-//        cityList.addCity(city2);
-//        cityList.addCity(city3);
-//
-//        assertEquals(4, cityList.countCities());
-//
-//        // Assert city not in list after deletion
-//        cityList.deleteCity(city3);
-//
-//        assertEquals(3, cityList.countCities());
-//        assertFalse(cityList.hasCity(city3));
-//    }
-//
-//    @Test
-//    void testDeleteCityException() {
-//        CustomList cityList = new CustomList();
-//        // Test to see if the exception is actually thrown
-//        City city = new City("Ottawa", "Ontario");
-//
-//        assertFalse(cityList.hasCity(city)); // Verify city not in list
-//
-//        assertThrows(NoSuchElementException.class, () -> {
-//            cityList.deleteCity(city);
-//        });
-//    }
+    @Test
+    void testDeleteCity() {
+        CustomList cityList = new CustomList();
+
+        // Test to see if deleteCity actually removes a city from the list
+        City city1 = new City("Calgary", "Alberta");
+        City city2 = new City("Regina", "Saskatchewan");
+        City city3 = new City("Charlottetown", "Prince Edward Island");
+
+        cityList.addCity(city1);
+        cityList.addCity(city2);
+        cityList.addCity(city3);
+
+        assertEquals(3, cityList.countCities());
+
+        // Assert city not in list after deletion
+        cityList.deleteCity(city3);
+
+        assertEquals(2, cityList.countCities());
+        assertFalse(cityList.hasCity(city3));
+    }
+
+    @Test
+    void testDeleteCityException() {
+        CustomList cityList = new CustomList();
+        // Test to see if the exception is actually thrown
+        City city = new City("Ottawa", "Ontario");
+
+        assertFalse(cityList.hasCity(city)); // Verify city not in list
+
+        assertThrows(NoSuchElementException.class, () -> {
+            cityList.deleteCity(city);
+        });
+    }
 
 
 }
